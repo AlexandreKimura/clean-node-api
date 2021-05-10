626c5108d8ddbf967d56cf23914331267a71f22e
Alexandre Kimura <alexandremkimura@hotmail.com>
2021-05-09 23:44:23 -0300

    test: ensure SignupController returns 400 if no name is provided

---
+++ src/presentation/controller/signup.spec.ts
@@ -0,0 +1,17 @@
+import { SignUpController } from './signup'
+
+describe('SignUp Controller', () => {
+  test('Should return 400 if no name is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        // Sem name
+        email: 'alexandre@teste.com',
+        password: '123456',
+        passwordConfirmation: '123456'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
